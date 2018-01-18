abcd90b9427f0da72f14c27f6bf28a462719eac5
iryna holovan <iholovan@luxoft.com>
2018-01-18 16:41:28 +0200

    Remittance contract updated

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,61 +1,102 @@
 
-pragma solidity ^0.4.19;
+pragma solidity 0.4.19;
 
 contract Remittance {
-    
-    bytes32 recipientAddressHash;
-    bytes32 recipientPasswordHash;
-    
-    uint256 public createdTime;
-    uint256 public validMinutes;
-    
+
+    bytes32 beneficiaryHashedPassword;
+    bytes32 remitterHashedPassword;
     address owner;
-    
-    function Remittance(address recipient, string password, uint256 expirationAfterMinutes) payable public {
-        require(recipient != address(0));
-        require(bytes(password).length != 0);
-        recipientAddressHash = keccak256(recipient);
-        recipientPasswordHash = keccak256(password);
+    uint deadline;
+    uint amount;
+
+    struct RemittanceBuilder{
+        uint remittanceAmount;
+        address moneyTransmitterAddress;
+        uint remittanceDeadline;
+    }
+
+    function Remittance(uint _deadline) payable{
+        deadline = _deadline;
         owner = msg.sender;
-        if (expirationAfterMinutes != uint256(0)) {
-            require(expirationAfterMinutes > 0);
-            Remittance.validMinutes = expirationAfterMinutes;
-        }
-        Remittance.createdTime = now;
+        amount = msg.value;
     }
-    
-    function extract(string password) public {
-        require(recipientAddressHash == keccak256(msg.sender));
-        require(recipientPasswordHash == keccak256(password));
-        selfdestruct(msg.sender);
-    }
-    
-    function kill() public isOwner() {
-        require(isExpired() || !canExpire());
-        selfdestruct(owner);
-    }
-    
-    function canExpire() public constant returns (bool) {
-        return validMinutes > 0;
-    }
-    
-    function isExpired() public constant returns (bool) {
-        return canExpire() && getSecondsTillExpiration() == 0;
-    }
-    
-    function getSecondsTillExpiration() public constant returns (uint256) {
-        if(canExpire()) {
-            uint256 secondsTillExpiration = (createdTime + validMinutes * 60 - now);
-            if (secondsTillExpiration > 0) {
-                return secondsTillExpiration; 
-            }
-        }
-        return 0;
-    }
-    
-    modifier isOwner() {
+
+    mapping(uint => RemittanceBuilder) remittanceMap;
+    uint[] ids;
+    mapping(address => uint ) balances;
+
+    event LogRemittance(address from, address to, uint amount);
+    event LogWithdrawl(bytes32 beneficiaryHashedPassword, bytes32 remitterHashedPassword, uint amount);
+
+
+
+    modifier isOwned(){
         require(msg.sender == owner);
         _;
     }
-    
+
+    function getBeneficiaryPasswordHash(string beneficiaryPassword)  returns (bytes32){
+        require(bytes(beneficiaryPassword).length != 0);
+        beneficiaryHashedPassword = keccak256(beneficiaryPassword);
+        return beneficiaryHashedPassword;
+    }
+
+    function getRemitterPasswordHash(string remitterPassword)  returns (bytes32){
+        require(bytes(remitterPassword).length != 0);
+        remitterHashedPassword = keccak256(remitterPassword);
+        return remitterHashedPassword;
+    }
+
+
+    function withdraw(bytes32 beneficiaryHashedPw, bytes32 remitterHashedPw,
+    uint commission, address moneyTransmitterAddress, uint withdrawDeadline)
+    isOwned payable returns(bool success){
+         require(msg.value > commission);
+         require(deadline > withdrawDeadline);
+         require(beneficiaryHashedPw != bytes32(0));
+         require(remitterHashedPw != bytes32(0));
+         require(commission != uint(0));
+         require(withdrawDeadline != uint(0));
+
+        require(moneyTransmitterAddress != address(0));
+        require(beneficiaryHashedPassword == beneficiaryHashedPw);
+        require(remitterHashedPassword == remitterHashedPw);
+        moneyTransmitterAddress.send(msg.value + commission);
+        balances[msg.sender] -= msg.value + commission;
+        balances[moneyTransmitterAddress] += msg.value + commission;
+        LogWithdrawl(beneficiaryHashedPw, remitterHashedPw, msg.value + commission);
+        return true;
+    }
+
+    function remit(address beneficiaryAddress, uint id, address moneyTransmitter,
+    uint remittanceDeadline, bytes32 beneficiaryHashedPw)
+    payable returns (bool success){
+        require(beneficiaryAddress != address(0));
+        require(id != uint(0));
+        require(moneyTransmitter != address(0));
+        require(remittanceDeadline != uint(0));
+        require(deadline > remittanceDeadline);
+        require(beneficiaryHashedPassword == beneficiaryHashedPw);
+        remittanceMap[id] = RemittanceBuilder(msg.value, moneyTransmitter, remittanceDeadline);
+        ids.push(id);
+        balances[moneyTransmitter] -= msg.value;
+        balances[beneficiaryAddress] += msg.value;
+        LogRemittance(moneyTransmitter, beneficiaryAddress, msg.value);
+        return true;
+    }
+
+    function getBalance(address clientAddress) payable returns(uint amount){
+        return balances[clientAddress];
+    }
+
+    function getRemittanceById(uint id) returns(uint remittanceAmount,
+    address moneyTransmitterAddress, uint remittanceDeadline){
+        RemittanceBuilder remittanceBuilder = remittanceMap[id];
+        return (
+            remittanceBuilder.remittanceAmount,
+            remittanceBuilder.moneyTransmitterAddress,
+            remittanceBuilder.remittanceDeadline
+        );
+    }
+
 }
