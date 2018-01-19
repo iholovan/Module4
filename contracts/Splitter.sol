1d31bf7b3c763b0f322021fa3c62503e204d1b69
iryna holovan <iholovan@luxoft.com>
2018-01-19 16:52:17 +0200

    Splitter contract update

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,42 +2,36 @@ pragma solidity ^0.4.19;
 
 contract Splitter {
     
+  address public recipient1Address;
+  address public recipient2Address;
   address public owner;
   uint public amount;
   uint remainder;
   mapping (address => uint) public balances;
-   
-  event LogSplitSent(address sender, address recipient1, address recipient2, uint amountSent);
+
+  event LogSplitSent(address sender, address recipient1, address recipient2, uint amountSent, uint totalAmout);
   event LogGetBalance(address recipient, uint amount);
-   
+
    function Splitter(address recipient1, address recipient2)  payable {
       require(recipient1 != 0);
       require(recipient2 != 0);
+      recipient1Address = recipient1;
+      recipient2Address = recipient2;
       owner = msg.sender;
-      remainder = msg.value % 2;
-      amount = msg.value / 2;
    }
-   
+
    modifier onlyOwner{
        require(msg.sender == owner);
        _;
    }
-    modifier notNull(uint value){
-        require(value > 0);
-        _;
-    }
-   
-  function sendMoney(address recipient1, address recipient2) 
+
+  function sendMoney()
   onlyOwner payable returns(bool success){
-      balances[recipient1] += amount + remainder;
-      balances[recipient2] += amount;
-      LogSplitSent(owner, recipient1, recipient2, msg.value/2);
+      remainder = msg.value % 2;
+      amount = msg.value / 2;
+      balances[recipient1Address] += amount + remainder;
+      balances[recipient2Address] += amount;
+      LogSplitSent(owner, recipient1Address, recipient2Address, amount, msg.value);
       return true;
   }
-  
-  function getBalance(address customerAddress) payable returns (uint){
-      LogGetBalance(customerAddress, balances[customerAddress]);
-      return balances[customerAddress];
-  }
-  
 }
