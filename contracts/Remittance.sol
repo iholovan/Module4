66a8d47d2d3e6ff2b400440b3e7b92fd2774da9b
iryna holovan <iholovan@luxoft.com>
2018-01-22 17:36:10 +0200

    Remittance contract update

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,101 +1,127 @@
-pragma solidity ^0.4.19;
+pragma solidity 0.4.19;
 
 contract Remittance {
 
-    bytes32 beneficiaryHashedPassword;
-    bytes32 remitterHashedPassword;
-    address owner;
-    uint deadline;
-    uint amount;
+    bytes32 private beneficiaryHashedPassword = 0xd533f44a496ce998e51ca2c0bbe9445fa36bd71ab9d39735f92339b368a617f1;
+    bytes32 private remitterHashedPassword = 0xffae97a8a972adf9bb9c72f22db8d7d687bcf1ed466105c78dccd16ac22d84ca;
+    address public owner;
+    uint public amount;
+    uint public deadline;
+
 
     struct RemittanceBuilder{
         uint remittanceAmount;
         address moneyTransmitterAddress;
-        uint remittanceDeadline;
     }
 
-    function Remittance(uint _deadline) {
-        deadline = _deadline;
-        owner = msg.sender;
-        amount = msg.value;
-    }
+    function Remittance(uint durationInMinutes)
+        public
+        {
+            owner = msg.sender;
+            deadline = now + durationInMinutes * 1 minutes;
+        }
 
-    mapping(uint => RemittanceBuilder) remittanceMap;
+    mapping(uint => RemittanceBuilder) public remittanceMap;
+
     uint[] ids;
-    mapping(address => uint ) balances;
+
+    mapping(address => uint) public balances;
 
     event LogRemittance(address from, address to, uint amount);
-    event LogWithdrawl(bytes32 beneficiaryHashedPassword, bytes32 remitterHashedPassword, uint amount);
+
+    event LogWithdrawl(uint sum);
+
+    modifier isOwned()
+        {
+            require(msg.sender == owner);
+            _;
+        }
+
+    modifier afterDeadline()
+        {
+            require(deadline >= now);
+            _;
+        }
+
+    function setBeneficiaryPasswordHash(
+        string beneficiaryPassword)
+        private
+        returns (bytes32 hash)
+        {
+            beneficiaryHashedPassword = keccak256(beneficiaryPassword);
+            return beneficiaryHashedPassword;
+        }
+
+    function setRemitterPasswordHash(
+        string remitterPassword)
+        private
+        returns (bytes32 hash)
+        {
+            remitterHashedPassword = keccak256(remitterPassword);
+            return remitterHashedPassword;
+        }
+
+    function deposit()
+        isOwned
+        public
+        payable
+        returns (bool success){
+            require(msg.value > 0);
+
+            amount = msg.value;
+
+            balances[msg.sender] -= msg.value;
+
+            return true;
+        }
 
 
+    function withdraw(
+        string beneficiaryPassword,
+        string remitterPassword,
+        address moneyTransmitterAddress)
+            afterDeadline
+            public
+            payable
+            returns(bool success)
+            {
+                require(beneficiaryHashedPassword == keccak256(beneficiaryPassword));
+                require(remitterHashedPassword == keccak256(remitterPassword));
+                require(moneyTransmitterAddress != 0);
 
-    modifier isOwned(){
-        require(msg.sender == owner);
-        _;
+                moneyTransmitterAddress.transfer(amount);
+
+                balances[moneyTransmitterAddress] += amount;
+
+                LogWithdrawl(amount);
+
+                return true;
+            }
+
+
+    function remit(
+        address beneficiaryAddress,
+        address moneyTransmitter,
+        uint id,
+        string beneficiaryPw)
+        public
+        payable
+        returns (bool success){
+            require(beneficiaryAddress != address(0));
+            require(moneyTransmitter != address(0));
+            require(id != uint(0));
+            require(beneficiaryHashedPassword == setBeneficiaryPasswordHash(beneficiaryPw));
+
+            remittanceMap[id] = RemittanceBuilder(amount, moneyTransmitter);
+
+            ids.push(id);
+
+            balances[beneficiaryAddress] += amount;
+            balances[moneyTransmitter] -= amount;
+
+            LogRemittance(moneyTransmitter, beneficiaryAddress, amount);
+
+            return true;
     }
 
-    function getBeneficiaryPasswordHash(string beneficiaryPassword)  returns (bytes32){
-        require(bytes(beneficiaryPassword).length != 0);
-        beneficiaryHashedPassword = keccak256(beneficiaryPassword);
-        return beneficiaryHashedPassword;
-    }
-
-    function getRemitterPasswordHash(string remitterPassword)  returns (bytes32){
-        require(bytes(remitterPassword).length != 0);
-        remitterHashedPassword = keccak256(remitterPassword);
-        return remitterHashedPassword;
-    }
-
-
-    function withdraw(bytes32 beneficiaryHashedPw, bytes32 remitterHashedPw,
-    uint commission, address moneyTransmitterAddress, uint withdrawDeadline)
-    isOwned payable returns(bool success){
-         require(msg.value > commission);
-         require(deadline > withdrawDeadline);
-         require(beneficiaryHashedPw != bytes32(0));
-         require(remitterHashedPw != bytes32(0));
-         require(commission != uint(0));
-         require(withdrawDeadline != uint(0));
-
-        require(moneyTransmitterAddress != address(0));
-        require(beneficiaryHashedPassword == beneficiaryHashedPw);
-        require(remitterHashedPassword == remitterHashedPw);
-        moneyTransmitterAddress.send(msg.value + commission);
-        balances[msg.sender] -= msg.value + commission;
-        balances[moneyTransmitterAddress] += msg.value + commission;
-        LogWithdrawl(beneficiaryHashedPw, remitterHashedPw, msg.value + commission);
-        return true;
-    }
-
-    function remit(address beneficiaryAddress, uint id, address moneyTransmitter,
-    uint remittanceDeadline, bytes32 beneficiaryHashedPw)
-    payable returns (bool success){
-        require(beneficiaryAddress != address(0));
-        require(id != uint(0));
-        require(moneyTransmitter != address(0));
-        require(remittanceDeadline != uint(0));
-        require(deadline > remittanceDeadline);
-        require(beneficiaryHashedPassword == beneficiaryHashedPw);
-        remittanceMap[id] = RemittanceBuilder(msg.value, moneyTransmitter, remittanceDeadline);
-        ids.push(id);
-        balances[moneyTransmitter] -= msg.value;
-        balances[beneficiaryAddress] += msg.value;
-        LogRemittance(moneyTransmitter, beneficiaryAddress, msg.value);
-        return true;
-    }
-
-    function getBalance(address clientAddress) payable returns(uint amount){
-        return balances[clientAddress];
-    }
-
-    function getRemittanceById(uint id) returns(uint remittanceAmount,
-    address moneyTransmitterAddress, uint remittanceDeadline){
-        RemittanceBuilder remittanceBuilder = remittanceMap[id];
-        return (
-            remittanceBuilder.remittanceAmount,
-            remittanceBuilder.moneyTransmitterAddress,
-            remittanceBuilder.remittanceDeadline
-        );
-    }
-
-}
+  }
--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -12,26 +12,31 @@ contract Splitter {
   event LogSplitSent(address sender, address recipient1, address recipient2, uint amountSent, uint totalAmout);
   event LogGetBalance(address recipient, uint amount);
 
-   function Splitter(address recipient1, address recipient2) {
-      require(recipient1 != 0);
-      require(recipient2 != 0);
-      recipient1Address = recipient1;
-      recipient2Address = recipient2;
-      owner = msg.sender;
-   }
+   function Splitter(
+        address recipient1,
+        address recipient2)
+        {
+            require(recipient1 != 0);
+            require(recipient2 != 0);
+            recipient1Address = recipient1;
+            recipient2Address = recipient2;
+            owner = msg.sender;
+        }
 
    modifier onlyOwner{
-       require(msg.sender == owner);
-       _;
-   }
+            require(msg.sender == owner);
+             _;
+        }
 
   function sendMoney()
-  onlyOwner payable returns(bool success){
-      remainder = msg.value % 2;
-      amount = msg.value / 2;
-      balances[recipient1Address] += amount + remainder;
-      balances[recipient2Address] += amount;
-      LogSplitSent(owner, recipient1Address, recipient2Address, amount, msg.value);
-      return true;
-  }
+    onlyOwner
+    payable
+    returns(bool success){
+            remainder = msg.value % 2;
+            amount = msg.value / 2;
+            balances[recipient1Address] += amount + remainder;
+            balances[recipient2Address] += amount;
+            LogSplitSent(owner, recipient1Address, recipient2Address, amount, msg.value);
+            return true;
+    }
 }
