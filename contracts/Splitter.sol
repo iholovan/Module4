67839c01f0fdc2aaf12c6d56f8763021d8547e69
iryna holovan <iholovan@luxoft.com>
2018-02-08 17:51:51 +0200

    Splitter contract update

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -6,41 +6,58 @@ contract Splitter {
   address public recipient2Address;
   address public owner;
   uint public amount;
-  uint remainder;
-  mapping (address => uint) public balances;
 
-  event LogSplitSent(
+  event LogSplit(
   address sender,
-  address recipient1,
-  address recipient2,
+  address recipient,
   uint amountSent,
   uint totalAmout);
 
-   function Splitter(
+
+    function Splitter(
         address recipient1,
         address recipient2)
+        payable
         {
             require(recipient1 != 0);
             require(recipient2 != 0);
+            require(msg.value > 0);
+            require(msg.value % 2 == 0);
             recipient1Address = recipient1;
             recipient2Address = recipient2;
             owner = msg.sender;
+            amount = msg.value / 2;
+
         }
 
-   modifier onlyOwner{
+    modifier onlyOwner{
             require(msg.sender == owner);
              _;
         }
 
-  function sendMoney()
-    onlyOwner
-    payable
-    returns(bool success){
-            require(msg.value % 2 == 0);
-            amount = msg.value / 2;
+    function sendMoneyToFirstAddress()
+        payable
+        public
+        returns(bool success){
+            require(amount > 0);
             recipient1Address.transfer(amount);
-            recipient2Address.transfer(amount);
-            LogSplitSent(owner, recipient1Address, recipient2Address, amount, msg.value);
+            LogSplit(msg.sender, recipient1Address, amount, msg.value);
             return true;
+        }
+
+    function sendMoneyToSecondAddress()
+        payable
+        public
+        returns(bool success){
+            require(amount > 0);
+            recipient2Address.transfer(amount);
+            LogSplit(msg.sender, recipient2Address, amount, msg.value);
+            return true;
+        }
+
+    function kill()
+        public
+        onlyOwner(){
+            selfdestruct(owner);
         }
 }
